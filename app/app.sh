b2db41a8ae41ba63682dcba040f10218cd52bc2b
mabittar <ma_bittar@yahoo.com.br>
2021-08-11 20:17:58 -0300

    fix app.sh local run

--- app/app.sh
+++ app/app.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-BINPATH=$(dirname $0)
-cd $BINPATH/..
-
 HOST='0.0.0.0'
 PORT='8000'
 WORKERS=1
-GUNICORN_WORKER_TIMEOUT=50
-LOCAL_ENV='/local.env'
+TIMEOUT=50
 
-APP='/app/main'
+APP='./app/main'
 
 if [ ! -z ${UVICORN_WORKERS} ]; then WORKERS=${UVICORN_WORKERS}; fi
 
 if [ "$1" = "--reload" ]
 then
     echo "--reload option enabled"
-    gunicorn -k uvicorn.workers.UvicornWorker --forwarded-allow-ips="*" --timeout-keep-alive ${GUNICORN_WORKER_TIMEOUT} --log-level 'warning' -w ${WORKERS} -b ${HOST}:${PORT} ${APP}
+    python -B ${APP}.py
 else
-    gunicorn -k uvicorn.workers.UvicornWorker --forwarded-allow-ips="*" --timeout-keep-alive ${GUNICORN_WORKER_TIMEOUT} --log-level 'warning' -w ${WORKERS} -b ${HOST}:${PORT} ${APP}
+    gunicorn -k uvicorn.workers.UvicornWorker --threads 3 --timeout 0 --graceful-timeout ${TIMEOUT} --keep-alive ${TIMEOUT} --forwarded-allow-ips="*" --log-level 'warning' -w ${WORKERS} -b ${HOST}:${PORT} ${APP}
 fi
